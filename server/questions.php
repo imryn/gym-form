6975fb8927639a10ab47a39e4969d8d27716e12c
imryn <noyimry@gmail.com>
2018-06-27 22:00:48 +0300

    fix 2 questions to save in db

--- server/questions.php
+++ server/questions.php
@@ -28,11 +28,11 @@
             $values = "'{$_SESSION['userid']}','{$_POST['bicycle']}','{$_POST['goingtogym']}','{$_POST['martialarts']}','{$_POST['game']}','{$_POST['running']}',
             '{$_POST['swimming']}','{$_POST['unoraerobic_exercises']}','{$_POST['gymsport']}','{$_POST['training_frequency']}',
             '{$_POST['training_favorite_time']}','{$_POST['balance']}','{$_POST['cardio']}','{$_POST['shaping_and_toning']}','{$_POST['weight_loss']}','{$_POST['goal']}',
-            '{$_POST['trainning_manner']}','{$_POST['trainning_cost']}'";
+            '{$_POST['trainning_manner']}','{$_POST['trainning_cost']}','{$_POST['food']}','{$_POST['trainning_satisfied']}'";
 
             $sql = "INSERT INTO questions (userid,bicycle,goingtogym,martialarts,game,running,swimming,unoraerobic_exercises,
             gymsport,training_frequency,training_favorite_time,balance,cardio,
-            shaping_and_toning,weight_loss,goal,trainning_manner,trainning_cost) VALUES ($values)";
+            shaping_and_toning,weight_loss,goal,trainning_manner,trainning_cost,food,trainning_satisfied) VALUES ($values)";
         
             $result =$this->db->query($sql);
             if($result){
