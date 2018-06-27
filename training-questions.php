747df79e42185d73f07063100b83dd8be3e713d0
imryn <noyimry@gmail.com>
2018-06-27 22:09:46 +0300

    fix h1 name

--- training-questions.php
+++ training-questions.php
@@ -37,7 +37,7 @@
     <div class="container">
       <section id="gym-questions">
             <form  action="/gym-form/server/api.php" method="post">
-                <h1> Questionnaire for Trainee  </h1>
+                <h1> Questionnaire for Trainer  </h1>
                 <div class="error">
                     <?php 
                     if( isSet($_GET['error-message']) ){
