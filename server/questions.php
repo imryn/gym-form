653a405bea79bde8b7af2bc617780381554eb6eb
imryn <noyimry@gmail.com>
2018-06-29 15:48:12 +0300

    fix query

--- server/questions.php
+++ server/questions.php
@@ -25,13 +25,13 @@
         public function createQuestionsFormForUser(){
             $this->allowSpecialCharacters($_POST);
 
-            $values = "'{$_SESSION['userid']}','{$_POST['bicycle']}','{$_POST['martialarts']}','{$_POST['game']}','{$_POST['running']}',
-            '{$_POST['swimming']}','{$_POST['unoraerobic_exercises']}','{$_POST['gymsport']}','{$_POST['training_frequency']}',
+            $values = "'{$_SESSION['userid']}','{$_POST['gymsport']}','{$_POST['bicycle']}','{$_POST['martialarts']}','{$_POST['game']}','{$_POST['running']}',
+            '{$_POST['swimming']}','{$_POST['unoraerobic_exercises']}','{$_POST['training_frequency']}',
             '{$_POST['training_favorite_time']}','{$_POST['balance']}','{$_POST['cardio']}','{$_POST['shaping_and_toning']}','{$_POST['weight_loss']}','{$_POST['goal']}',
             '{$_POST['trainning_manner']}','{$_POST['trainning_cost']}','{$_POST['food']}','{$_POST['trainning_satisfied']}'";
 
-            $sql = "INSERT INTO questions (userid,bicycle,martialarts,game,running,swimming,unoraerobic_exercises,
-            gymsport,training_frequency,training_favorite_time,balance,cardio,
+            $sql = "INSERT INTO questions (userid,gymsport,bicycle,martialarts,game,running,swimming,unoraerobic_exercises,
+            training_frequency,training_favorite_time,balance,cardio,
             shaping_and_toning,weight_loss,goal,trainning_manner,trainning_cost,food,trainning_satisfied) VALUES ($values)";
         
             $result =$this->db->query($sql);
